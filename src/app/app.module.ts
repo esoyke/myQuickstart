70361573bca73629f8a5d0a1e1e414419c24d058
Eric Soyke <ESoyke@pscu.com>
2017-11-06 15:52:40 -0500

    data binding stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+// EBS had to add import of FormsModule to get 2-way binding from forms working (banana in a box notation ->  [(ngModel)] )
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
